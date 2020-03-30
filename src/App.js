db6f4673657421645f7ab37e5fcacfd6d1574ead
Yoann Paquereau <yoann.paquereau@gmail.com>
2020-03-30 15:46:35 +0200

    link between react and node change navbar when login or not

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.scss';
 import Navbar from "./components/layout/Navbar";
+import { Provider } from 'react-redux';
+import store from './components/store';
+
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './components/utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './components/actions/authActions';
+
+const state = {
+  isAuthentificated: false
+};
+
+
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+
+  const decoded = jwt_decode(token);
+  store.dispatch(setCurrentUser(decoded));
+
+
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+
+    window.location.href = './login';
+  }
+  else {
+    state.isAuthentificated = true;
+  }
+}
 
 function App() {
   return (
-    <Navbar/>
+    <Provider store={store}>
+      <Navbar auth={state.isAuthentificated} />
+    </Provider>
   );
 }
 
